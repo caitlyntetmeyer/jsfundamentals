12659271f83327268fbd5e5aaa213aadbddc6b56
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-06-19 13:23:17 -0400

    Added IIFE notes and 0.11.2_installing_node.js

--- 0.15_iife.js
+++ 0.15_iife.js
@@ -4,3 +4,34 @@
 		console.log(i);
 	}
 })();
+
+/* 
+Fix this code:
+
+var smartCar = {;
+var smartCarMPG = function(totalMiles, totalGallons){
+	return{
+		calculateMpg : function(){
+			console.log("MPG: ");
+			return totalMiles / totalGallons;
+		}
+	};
+};
+
+smartCar.mpg = smartCarMPG(____, 15);
+consoLe.log(smartCar.mpg);
+console.Log(smartCar._____.calculateMpg());
+*/
+
+
+var smartCar = {};
+var smartCarMPG = function(totalMiles, totalGallons) {
+		calculateMpg = function(){
+			console.log("MPG: " + (totalMiles/totalGallons));
+	};
+};
+
+smartCarMPG(230, 15);
+
+
+
