27f6e73a42e2913ab88b3bf844610c59c6908f52
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-07-10 18:58:51 -0400

    Added notes on what to learn next

---
+++ NetNinjaJavaScript/20_Next-Steps.js
@@ -0,0 +1,28 @@
+/* 
+
+What to Do Next
+
+https://www.youtube.com/watch?v=Qikr22Tc3wg&index=46&list=PL4cUxeGkcC9i9Ae2D9Ee1RvylH38dKuET 
+
+JavaScript Libraries:
+jQuery
+MooTools (not quite as popular)
+Modernizr (more to do w/keeping up w/browser standards)
+
+AJAX:
+essentially a part of JavaScript
+Allows JS to communicate w/the server w/o having to leave the web page
+Ex: When you zoom in on Google Maps
+
+JSON:
+JavaScript Object Notation
+Way of organizing data; works well w/AJAX
+
+More JavaScript:
+Regular expressions & advanced validation (ex: make sure a user entered her email correctly instead of just some random word)
+OOP (object oriented programming)
+Projects with sliders, drag and drop, animated menus
+
+Watch the Net Ninja YouTube channel!
+
+*/
