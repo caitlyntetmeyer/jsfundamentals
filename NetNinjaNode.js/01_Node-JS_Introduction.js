b01234c8fa79720a9ca5058d966bba930b6332f8
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-07-18 09:28:59 -0400

    Started Net Ninja Node-js tutorials

---
+++ NetNinjaNode.js/01_Node-JS_Introduction.js
@@ -0,0 +1,23 @@
+/*
+
+Node.js
+-platform that lets us run JavaScript on a computer/server
+-read, delete & update files
+-easily communicate w/a database
+
+Video: https://www.youtube.com/watch?v=w-7RQ46RgxU&index=1&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp
+
+Why Node.js?
+-uses JS
+-very fast (runs on the V8 engine & uses non-blocking code)
+-huge ecosystem of open-source packages (npm)
+-great for real-time chats
+
+3 sections for this series:
+-inner workings of Node.js: V8 engine, modules, event emitter, the file system
+-creating a web server: routing, Express, templating
+-make a Node.js application (todo app)
+
+
+
+*/
