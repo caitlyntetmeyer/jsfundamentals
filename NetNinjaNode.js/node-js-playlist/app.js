a6c21075ea36f73a855f527728ca28ba569d3262
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-07-19 10:30:14 -0400

    Working through the Net Ninja Node.js tutorials

---
+++ NetNinjaNode.js/node-js-playlist/app.js
@@ -0,0 +1,28 @@
+/*
+
+Net Ninja Node.js Tutorials #1 & #2:
+
+#1 video: https://www.youtube.com/watch?v=w-7RQ46RgxU&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp&index=1
+
+#2 video: https://www.youtube.com/watch?v=1US-P13yKVs&index=2&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp
+
+*/
+console.log('Hey ninjas');
+
+/* #3 Node.js video: The V8 Engine  https://www.youtube.com/watch?v=86tgU7UaJmU&index=3&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp
+
+JavaScript Engines
+-Computers don't understand JavaScript.
+-A JavaScript engine takes JS and converts it into sth computers do understand: machine code.
+
+Machine Code Diagram:
+High-level: JavaScript (abstracted a lot away from machine code)
+Next down: C++ (powerful)
+Next down: Assembly Language (abstracted a little bit away from machine code)
+Lowest-level: Machine Code (what computers understand - really hard for humans to understand)
+
+So we need a way for JS to be converted into Machine Code.
+Node.js is actually written in C++ - crazy, right? But as a program, it allows us to write JS that can run on a computer or server. Node.js uses the V8 JS engine by Google that's also written in C++.
+
+In conclusion, at the heart of Node.js is the V8 engine, which converts our JS into machine code.
+*/
