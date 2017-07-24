5157d4fac7b7be9b6d2d71c4f24a800a367eaa8e
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-07-24 09:14:22 -0400

    Updated notes on function expressions

--- NetNinjaNode.js/05_Function-Expressions.js
+++ NetNinjaNode.js/05_Function-Expressions.js
@@ -1,44 +1,44 @@
-/*
+	/*
 
-Node.js - open-source, cross-platform JavaScript runtime environment for 
-executing JavaScript code server-side
+	Node.js - open-source, cross-platform JavaScript runtime environment for
+	executing JavaScript code server-side
 
-Function Expressions in Node.js
+	Function Expressions in Node.js
 
-https://www.youtube.com/watch?v=lK42xIMcA0Y
+	Video #5: https://www.youtube.com/watch?v=lK42xIMcA0Y
 
----
+	---
 
-First, an example of a vanilla JS function:
+	First, an example of a vanilla JS function:
 
-Declare it:
-*/
-function sayHi(){
-	console.log("Hi");
-}
+	Declare it:
+	*/
+	function sayHi(){
+		console.log("Hi");
+	}
 
-// Call it:
-sayHi();		// Hi
-/*
+	// Call it:
+	sayHi();		// Hi
+	/*
 
----
+	---
 
-A function expression is a variable equal to an anonymous function:
-*/
-var sayBye = function(){
-	console.log("Bye");
-};
-sayBye();		// Bye
+	A function expression is a variable equal to an anonymous function:
+	*/
+	var sayBye = function(){
+		console.log("Bye");
+	};
+	sayBye();		// Bye
 
-/*
-Pattern often used in Node.js: Passing a function into another function
+	/*
+	Pattern often used in Node.js: Passing a function into another function
 
-Below, the function callFunction() takes the parameter fun. 
-We can pass another function, sayBye(), in as the argument:
-*/
-function callFunction(fun){
-	fun();
-};
-// Call it:
-callFunction(sayBye);	// Bye
-// We just passed a function into a function!
+	Below, the function callFunction() takes the parameter fun.
+	We can pass another function, sayBye(), in as the argument:
+	*/
+	function callFunction(funct){
+		funct();
+	};
+	// Call it:
+	callFunction(sayBye);	// Bye
+	// We just passed a function into a function!
