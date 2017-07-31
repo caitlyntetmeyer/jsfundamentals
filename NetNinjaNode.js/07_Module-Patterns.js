23cbccfa7662688c84f0c5878540a4d293d6be37
Caitlyn Tetmeyer <caitlyntetmeyer@gmail.com>
2017-07-31 19:06:53 -0400

    Completed notes on modules patterns

--- NetNinjaNode.js/07_Module-Patterns.js
+++ NetNinjaNode.js/07_Module-Patterns.js
@@ -16,10 +16,53 @@ var adder = function(a,b){
 
 var pi = 3.142;
 
+// stuff is module.exports.
+// .counter is a PROPERTY:
 module.exports.counter = counter;
+// .adder is a PROPERTY:
+module.exports.adder = adder;
+// .pi is a PROPERTY:
+module.exports.pi = pi;
+/*
 
-// 2) In app.js:
+2) In app.js:
 
-var counter = require('./stuff'); // requires stuff.js in this file
+*/
+var stuff = require('./stuff'); // requires stuff.js in this file
 
-console.log(counter(['shaun', 'crystal', 'ryu']));
+console.log(stuff.counter(['shaun', 'crystal', 'ryu'])); // There are 3 elements in this array
+console.log(stuff.adder(5,6)); // The sum of the 2 numbers is 11
+console.log(stuff.adder(stuff.pi,5); // The sum of the 2 numbers is 8.142
+/*
+
+3) Easier way: Make stuff.js this way instead -
+*/
+module.exports.counter = function(arr) {
+  return 'There are ' + arr.length + ' elements in this array!';
+};
+
+module.exports.adder = function(a,b){
+  return `The sum of the 2 numbers is ${a+b}`
+};
+
+module.exports.pi = 3.142;
+/*
+
+A third way you could do stuff.js:
+*/
+var counter = function(arr) {
+  return 'There are ' + arr.length + ' elements in this array!';
+};
+
+var adder = function(a,b){
+  return `The sum of the 2 numbers is ${a+b}`
+};
+
+var pi = 3.142;
+
+// Using Object Literal notation:
+module.exports = {
+  counter: counter,
+  adder: adder,
+  pi: pi
+}
