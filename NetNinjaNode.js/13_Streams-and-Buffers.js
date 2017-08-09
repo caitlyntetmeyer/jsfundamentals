a866c1841e0696a74032ebac317b0609fc5057d3
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-08-09 16:45:25 -0400

    continued notes

--- NetNinjaNode.js/13_Streams-and-Buffers.js
+++ NetNinjaNode.js/13_Streams-and-Buffers.js
@@ -1,2 +1,10 @@
 // Node.js Tutorial: Streams and Buffers
+
 // Net Ninja Video #13: https://www.youtube.com/watch?v=GlybFFMXXmQ&index=13&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp
+
+// Topic:
+// How streams and buffers work, so that we're fully prepared to use them within our node application.
+
+// Streams and buffers:
+// 1) allow data to be consumed before it's fully downloaded
+// 2) increase the performance of your app.
