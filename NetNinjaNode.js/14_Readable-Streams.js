5f232d74a5e58c6ad2f3addcfe08e2609abf4fa4
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-08-09 16:52:15 -0400

    continued notes

--- NetNinjaNode.js/14_Readable-Streams.js
+++ NetNinjaNode.js/14_Readable-Streams.js
@@ -4,7 +4,13 @@ Readable Streams
 
 Net Ninja Node.js Video #14: https://www.youtube.com/watch?v=E3tTzx0Qoj0&index=13&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp#t=42.961568
 
+writable streams - allow node js to write data to a stream
 
+readable streams - allow node js to read data from a streams
+
+duplex - can read and write to a streams
+
+Left off at 1:38 in the video.
 
 
 */
