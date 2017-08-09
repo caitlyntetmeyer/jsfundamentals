0e4a2b71df961469e7b06fb92d22afa5bdf4cfc7
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2017-08-09 16:48:57 -0400

    Completed notes on streams and buffers

--- NetNinjaNode.js/13_Streams-and-Buffers.js
+++ NetNinjaNode.js/13_Streams-and-Buffers.js
@@ -8,3 +8,13 @@
 // Streams and buffers:
 // 1) allow data to be consumed before it's fully downloaded
 // 2) increase the performance of your app.
+
+// Buffer - temp. storage spot for a chunk of data that's being transported
+// Purpose: While data is being transferred, you can transfer just a little bit of data at a time...the buffer gets filled...and the data's delivered.
+
+// Stream - data being passed on and processed
+// Think about streaming movies online. You just need SOME of the chunks of data to be delivered, and then you can start watching the movie. You don't have to gather ALL of the data in memory first.
+
+// Streams in Node.js:
+// 1. Can create streams in Node.js to transfer data
+// 2. Increase performance
