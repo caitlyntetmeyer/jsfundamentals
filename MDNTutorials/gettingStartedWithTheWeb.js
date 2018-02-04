f1f1de42b7d1fe266e888e5f193f7c4d5a4522ed
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-02-04 09:29:19 +0100

    Started test site

--- MDNTutorials/gettingStartedWithTheWeb.js
+++ MDNTutorials/gettingStartedWithTheWeb.js
@@ -4,8 +4,25 @@ MDN - Getting Started with the Web
 
 https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web
 
+First, plan your website: https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/What_will_your_website_look_like
 
+Decide on assets: text (Arial), theme color (#2A2A73), images (Mozilla icon)
 
+Color picker tool: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Colors/Color_picker_tool
+
+---
+
+The font I picked:
+
+HTML:
+
+<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+
+CSS:
+
+font-family: 'Oswald', sans-serif;
+
+---
 
 
 
