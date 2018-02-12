83e7bcfc10e697394ca220445d7cf649b0bbdec6
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-02-12 08:05:52 -0600

    3 types of declarations in JS: var, let, const

--- MDNTutorials/jsGuideIntro.js
+++ MDNTutorials/jsGuideIntro.js
@@ -2,10 +2,10 @@
 
 JavaScript Guide - Introduction:
 
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Introduction
+	https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Introduction
 
 
-The DOM (Document Object Model) defines the way in which HTML document objects are exposed to your script.
+	The DOM (Document Object Model) defines the way in which HTML document objects are exposed to your script.
 
 */
 
@@ -21,7 +21,18 @@ greetMe("Caitlyn"); // Hello, Caitlyn!
 
 JavaScript Grammar and Types:
 
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types
+	https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types
+
+
+	3 Kinds of Declarations in JavaScript:
+
+		1) var - declares a variable, optionally initializing it to a value
+
+		2) let - declares a block-scoped, local variable, optionally initializing it to a value
+
+		3) const - declares a block-scoped, read-only named constant
+
+	Variables:
 
 
 */
