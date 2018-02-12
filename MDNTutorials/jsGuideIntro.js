ff3d23edd5ab6d44c7574935fa091ff73f7d6003
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-02-12 08:01:40 -0600

    began JavaScript Guide Introduction on MDN

---
+++ MDNTutorials/jsGuideIntro.js
@@ -0,0 +1,29 @@
+/*
+
+JavaScript Guide - Introduction:
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Introduction
+
+
+The DOM (Document Object Model) defines the way in which HTML document objects are exposed to your script.
+
+*/
+
+// Ex. 1:
+
+function greetMe(yourName) {
+  alert("Hello, " + yourName + "!");
+}
+
+greetMe("Caitlyn"); // Hello, Caitlyn!
+
+/* 
+
+JavaScript Grammar and Types:
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types
+
+
+*/
+
+
