98c7de74e298d1a8ac54f4f7d7853a1ed865ee11
caitlyntetmeyer <caitlyntetmeyer@gmail.com>
2018-02-20 17:47:34 -0500

    Began conditional statements

--- MDNTutorials/jsControlFlowErrHandling.js
+++ MDNTutorials/jsControlFlowErrHandling.js
@@ -35,3 +35,63 @@ Any JS expression is also a statement.
 		console.log(x); // 2 - because both "var x" statements are in the same SCOPE.
 
 			// Starting with ECMAScript2015, the "let" variable declaration IS block-scoped.
+
+	// Conditional Statements:
+
+		// If Statement:
+
+		if (condition) {
+			statement_1;
+		} else {
+			statement_2;
+		}
+
+		// You can use "else if" to have multiple conditions tested in sequence:
+
+		if (condition_1) {
+			statement_1;
+		} else if (condition_2) {
+			statement_2;
+		} else if (condition_n) {
+			statement_n;
+		} else {
+			statement_last;
+		}
+
+		// Only the 1st logical condition that evaluates to true will be executed. To execute multiple statements, group them in a block statement:
+
+		if (condition) {
+			statement_1_runs_if_condition_is_true;
+			statement_2_runs_if_condition_is_true;
+		} else {
+			statement_3_runs_if_condition_is_false;
+			statement_4_runs_if_condition_is_false;
+		}
+
+	/*
+
+	Falsy Values:
+
+	*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
